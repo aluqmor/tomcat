a6e4a64251ade0a738399da464caf08c6fa568f3
Álvaro Luque <alvarolu99@gmail.com>
2025-01-20 09:55:54 +0100

    final

--- provision.sh
+++ provision.sh
@@ -3,14 +3,11 @@
 # Actualización de los paquetes disponibles
 sudo apt update && sudo apt upgrade -y
 
-# Instalación de OpenJDK 11
-sudo apt install -y openjdk-11-jdk
+# Instalación de OpenJDK 11, Tomcat 9 y Tomcat 9 Admin
+sudo apt install -y openjdk-11-jdk tomcat9 tomcat9-admin
 
-# Instalación de Tomcat 9
-sudo apt install -y tomcat9
-
-# Instalación de Tomcat 9 Admin
-sudo apt install -y tomcat9-admin
+# Instalación de Maven
+sudo apt-get update && sudo apt-get -y install maven
 
 # Creación del grupo para Tomcat
 sudo groupadd tomcat9 || true
@@ -32,6 +29,7 @@ sudo bash -c 'cat > /etc/tomcat9/tomcat-users.xml' <<EOF
   <user username="alumno"
         password="1234"
         roles="admin,admin-gui,manager,manager-gui"/>
+  <user username="deploy" password="1234" roles="manager-script"/>
 </tomcat-users>
 EOF
 
@@ -48,6 +46,135 @@ sudo bash -c 'cat > /usr/share/tomcat9-admin/host-manager/META-INF/context.xml' 
           $LruCache(?:\\$1)?|java\\.util\\.(?:Linked)?HashMap"/>
 </Context>
 EOF
+
+# Configuración del archivo settings.xml para Maven
+sudo bash -c 'cat > /etc/maven/settings.xml' <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                              https://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <servers>
+    <server>
+      <id>Tomcat</id>
+      <username>deploy</username>
+      <password>1234</password>
+    </server>
+  </servers>
+</settings>
+EOF
+
+# Reinicio del servicio Tomcat para aplicar cambios
+sudo systemctl restart tomcat9
+
+# Creación de un proyecto Maven
+mvn archetype:generate -DgroupId=org.zaidinvergeles -DartifactId=tomcat-war -Ddeployment -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+
+# Entrar en el directorio del proyecto
+cd tomcat-war
+
+# Modificacion del archivo pom.xml
+sudo bash -c 'cat > pom.xml' <<EOF
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.zaidinvergeles</groupId>
+  <artifactId>tomcat-war</artifactId>
+  <packaging>war</packaging>
+  <version>1.0-SNAPSHOT</version>
+  <name>tomcat-war Maven Webapp</name>
+  <url>http://maven.apache.org</url>
+  <dependencies>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>3.8.1</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+  <build>
+    <finalName>tomcat-war</finalName>
+	 <plugins>
+	 <plugin>
+	 <groupId>org.apache.tomcat.maven</groupId>
+	 <artifactId>tomcat7-maven-plugin</artifactId>
+	 <version>2.2</version>
+	 <configuration>
+	 <url>http://localhost:8080/manager/text</url>
+	 <server>Tomcat</server>
+	 <path>/despliegue</path>
+	 </configuration>
+	 </plugin>
+ 	</plugins>
+  </build>
+</project>
+EOF
+
+# Despliegue de la aplicacion
+mvn tomcat7:deploy
+
+# Volver a la carpeta raíz
+cd 
+
+# Instalación de Git
+sudo apt update && sudo apt install -y git
+
+# Clonar el repositorio de la aplicación
+git clone https://github.com/cameronmcnz/rock-paper-scissors.git
+
+# Entrar en el directorio de la aplicación
+cd rock-paper-scissors
+
+# Cambiar de rama
+git checkout patch-1
+
+# Modificacion del archivo pom.xml
+sudo bash -c 'cat > pom.xml' <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>com.mcnz.rps.web</groupId>
+  <artifactId>roshambo</artifactId>
+  <version>1.0</version>
+  <packaging>war</packaging>
+  <name>roshambo web application</name>
+  <url>http://www.mcnz.com</url>
+  <properties>
+    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+    <maven.compiler.source>1.7</maven.compiler.source>
+    <maven.compiler.target>1.7</maven.compiler.target>
+  </properties>
+  <dependencies>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>4.11</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+  <build>
+    <finalName>roshambo</finalName>
+    <pluginManagement><!-- lock down plugins versions to avoid using Maven defaults (may be moved to parent pom) -->
+       	<plugins>
+        	<plugin>
+            		<groupId>org.apache.tomcat.maven</groupId>
+            		<artifactId>tomcat7-maven-plugin</artifactId>
+            		<version>2.2</version>
+            		<configuration>
+                		<url>http://localhost:8080/manager/text</url>
+                		<server>Tomcat</server>
+                		<path>/rps</path>
+            		</configuration>
+        	</plugin>
+   	 </plugins>
+    </pluginManagement>
+  </build>
+</project>
+EOF
+
+# Despliegue de la aplicacion
+mvn tomcat7:deploy
 
 # Reinicio del servicio Tomcat para aplicar cambios
 sudo systemctl restart tomcat9
